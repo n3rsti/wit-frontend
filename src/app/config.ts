8921e5779dec19cc5baa0d21e5f156ea4b1054f0
n3rsti <n3rsti@protonmail.com>
2022-09-22 13:12:31 +0200

    create token interceptor

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 
-export const host = (window as any)['__env']['apiUrl'] || 'http://127.0.0.1:8080';
+export const host = 'http://127.0.0.1:8080';
 
 export const Config = {
   Host: host,
---
+++ src/app/modules/token-interceptor/token-interceptor.module.ts
@@ -0,0 +1,92 @@
+import {Injectable, ModuleWithProviders, NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+  HttpErrorResponse,
+  HttpResponse, HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import {Router} from "@angular/router";
+import {catchError, finalize, Observable, retry, tap, throwError} from "rxjs";
+import {Config} from "../../config";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
+export class TokenInterceptor implements HttpInterceptor {
+  whitelistedUrls = [
+    '/login',
+    '/logout'
+  ]
+
+  constructor(private router: Router) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token: string = localStorage.getItem('token') || '';
+    if (this.whitelistedUrls.includes(req.url.replace(Config.Host, ''))) {
+      return next.handle(req);
+    }
+
+    if (token != '') {
+      const reqWithToken = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(reqWithToken).pipe(
+        retry(2),
+        catchError((error: HttpErrorResponse) => {
+          return throwError(error)
+        }),
+        finalize(() => {
+
+        }),
+        tap((event: any) => {
+          if (event instanceof HttpResponse) {
+            console.log(event);
+            this.router.navigate(['login']);
+          }
+        })
+      );
+
+    } else {
+      this.router.navigate(['login']);
+    }
+
+    return next.handle(req);
+  }
+
+}
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    TokenInterceptor,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ]
+})
+export class TokenInterceptorModule {
+  static forRoot(): ModuleWithProviders<TokenInterceptorModule>{
+    return {
+      ngModule: TokenInterceptorModule,
+      providers: [
+        { provide: HTTP_INTERCEPTORS,
+          useClass: TokenInterceptor,
+          multi: true
+        }
+      ]
+    }
+  }
+}

bde4ab833f164305a836bb2e0f43b4e538f73b9a
n3rsti <n3rsti@protonmail.com>
2022-09-22 14:07:32 +0200

    create user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,37 @@
+export class IUser {
+  protected id: string = '';
+  protected username: string = '';
+
+  get Id(){
+    return this.id;
+  }
+
+  get Username(){
+    return this.username;
+  }
+}
+
+export class UserBuilder extends IUser{
+  constructor() {
+    super();
+  }
+
+  setUsername(username: string){
+    this.username = username;
+    return this;
+  }
+
+  setId(id: string){
+    this.id = id;
+    return this;
+  }
+
+}
+
+export class User extends IUser {
+  constructor(builder: UserBuilder) {
+    super();
+    this.id = builder.Id;
+    this.username = builder.Username;
+  }
+}

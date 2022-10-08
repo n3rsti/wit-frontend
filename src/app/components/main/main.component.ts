7d6669705f9aa2e7c6d8c0c636f3599d61b6ea32
n3rsti <n3rsti@protonmail.com>
2022-10-08 16:03:06 +0200

    create basic form for post creation

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from "../../services/data.service";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  postContent = '';
+
+  constructor(
+    private data: DataService
+  ) {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  createPost(){
+
   }
 
 }
